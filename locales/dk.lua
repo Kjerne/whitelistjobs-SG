a55433893ac5be524cbe5688122ee5cadd22768c
Kjerne <59027271+Kjerne@users.noreply.github.com>
2020-02-14 01:18:07 +0100

    Danish translation
    Still some who ain't translated

---
+++ locales/dk.lua
@@ -0,0 +1,18 @@
+Locales['en'] = {
+	['new_job'] = 'Du er nu gået på arbejde som politibetjent!',
+	['offduty_job'] = 'Du er nu gået af arbejde!',
+	['access_job_center'] = 'tryk ~INPUT_PICKUP~ for at gå på arbejde som ~b~Politibetjent~s~.',
+	['job_center'] = 'Gå på job som politibetjent',
+	['clock_out'] = 'Gå af arbejde',
+	['co_job_center'] = 'tryk ~INPUT_PICKUP~ for at gå af arbejdet.',
+	['lscs'] = 'LSCS',
+	['ambulance'] = 'Læge',
+	['mecano'] = 'Mekaniker',
+	['banker'] = 'Bankmand',
+	['mafia'] = 'Mafia',
+	['police'] = 'Politi',
+	['cartel'] = 'Cartel',
+	['realestateagent'] = 'Real Estate Agent',
+	['thelostmc'] = 'The Lost MC',
+
+}
